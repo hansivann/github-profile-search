65ff5ad21d2c61a5fb155bc3a1d0ea98adddc5bb
Ivan Francisco <hansivann23@gmail.com>
2023-12-16 01:09:07 -0800

    Add GitHub API integration to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,31 +4,58 @@
 // image, count for followers, following, # of repos,
 // top 5 repos
 // get github api and render it to card. Hide api source! 
+//  need
+// user_url, followers_url, following_url, user_repositories_url, user_repositories_url
+
+import { useEffect, useState } from "react";
+import {getSource} from '../api/script.js'
 
 export default function Card() {
+  const [userData, setUserData] = useState(null);
+
+  console.log(userData)
+
+  useEffect(() => {
+      getSource('bradtraversy').then(data => setUserData(data))
+  }, [])
+
   return (
     <>
-      <div className="flex flex-col items-center justify-center mt-6">Card goes here
-        <div className="flex flex-col items-center justify-center">Picture here
-          <div>username</div>
+      { userData &&
+        <div className="flex flex-col items-center justify-center mt-6">
+        <div className="flex flex-col items-center justify-center">
+          <div className="">{userData.avatar_url}</div>
+          <div >{userData.login}</div>
           <div className="links">
             <ul className=" flex flex-row w-max justify-center space-x-6">
-              <li>followers</li>
-              <li>following</li>
-              <li># repos</li>
+              <li>{userData.followers} followers</li>
+              <li>{userData.following} following</li>
+              <li>{userData.public_repos} public repos</li>
             </ul>
           <div className="top-repos">
             <ul className="flex w-max space-x-8 justify-between">
               <li>repo 1</li>
               <li>repo 2</li>
               <li>repo 3</li>
-              <li>repo 4</li>
-              <li>repo 5</li>
             </ul>
           </div>
           </div>
         </div>
       </div>
+      }
+
+{/* {userData && (
+        <div className="flex flex-col items-center justify-center mt-6">
+          <div>{userData.avatar_url}
+            {userData.login}</div>
+          <div className="links">
+            <ul className="flex flex-row w-max justify-center space-x-6">
+              <li>{userData.followers} followers</li>
+              <li>{userData.following} following</li>
+            </ul>
+          </div>
+        </div>
+      )} */}
     </>
   )
 }
