cf01608e5c57649020a156c704777803aa0560ea
Ivan Francisco <hansivann23@gmail.com>
2023-12-15 16:27:08 -0800

    Add skeleton for Card component and remove unused import

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+
+//TO-DO:
+// build skeleton for: 
+// image, count for followers, following, # of repos,
+// top 5 repos
+// get github api and render it to card. Hide api source! 
 
 export default function Card() {
   return (
-    <div>Card goes here</div>
+    <>
+      <div>Card goes here</div>
+    </>
   )
 }
+
